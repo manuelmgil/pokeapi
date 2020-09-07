95ab44c0cd76f83c71445ea1dfa23776b436878d
Manuel Mukul <manuelmgil.94@gmail.com>
2020-09-06 22:11:33 -0500

    Updates

---
+++ precache-manifest.91a7f6015caf407c0a9589e66c9dc600.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e655841bbfd1fc1c36c7e1ccdaaad879",
+    "url": "/pokeapi/index.html"
+  },
+  {
+    "revision": "42dba1c27413c2383d97",
+    "url": "/pokeapi/static/css/main.3a4b6fbc.chunk.css"
+  },
+  {
+    "revision": "3f8d3d3b4a2d439f1248",
+    "url": "/pokeapi/static/js/2.8acd1713.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pokeapi/static/js/2.8acd1713.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42dba1c27413c2383d97",
+    "url": "/pokeapi/static/js/main.0aa8c2e3.chunk.js"
+  },
+  {
+    "revision": "c4c0ce2684cf0efed64a",
+    "url": "/pokeapi/static/js/runtime-main.b7303536.js"
+  }
+]);
